1653cbb8856d9c4a803801c52db80c749bf8ee2f
Ray Mercedes <contact@RayMercedes.com>
2019-07-11 00:32:47 -0400

    index.html updated, grunt sass not implemented yet

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,66 +1,70 @@
-module.exports = function(grunt) {
+module.exports = function (grunt) {
 
   // Project configuration.
   grunt.initConfig({
     concat: {
       js: {
-        src: ['./library/js/script.js', './library/js/script1.js', './library/js/script2.js', './library/js/script3.js'],
-        dest: 'build/JS/Scripts.js',
+        src: ['./library/js/script.js'],
+        dest: 'dist/JS/main.js',
       },
       css: {
         src: ['./library/css/custom_styles.css', './library/css/style.css', './library/css/mobile.css'],
-        dest: 'build/CSS/Styles.css',
-      },
-
+        dest: 'dist/CSS/Styles.css',
+      }
     },
+
+    sass: { // Task
+      dist: { // Target
+        options: { // Target options
+          style: 'expanded'
+        },
+        files: { // Dictionary of files
+          'main.css': 'main.scss' // 'destination': 'source'
+        }
+      }
+    },
+
+    // sass: {
+    //   build: {
+    //     files: [{
+    //           //       '/library/SASS/styles.scss': '/dist/CSS/style.css'
+
+    //       // expand: true,
+    //       // cwd: 'styles',
+    //       // src: ['./library/sass/*.sass'],
+    //       // dest: './dist/CSS/',
+    //       // ext: '.css'
+    //     }]
+    //   }
+    // },
+
+    // sass: {
+    //   dist: {
+    //     files: [{
+    //       '/library/SASS/styles.scss': '/dist/CSS/style.css'
+    //     }]
+    //   }
+    // },
+
+
     watch: {
       js: {
-        files: ['./JS/**/*.js'],
+        files: ['./library/JS/**/*.js'],
         tasks: ['concat'],
       },
       css: {
-        files: ['./CSS/**/*.css'],
+        files: ['./library/CSS/**/*.css'],
         tasks: ['concat'],
       },
-    },
-
-  //   sass: {                              // Task
-  //     dist: {                            // Target
-  //       options: {                       // Target options
-  //         style: 'expanded'
-  //       },
-  //       files: {                         // Dictionary of files
-  //         'main.css': 'main.scss',       // 'destination': 'source'
-  //         'widgets.css': 'widgets.scss'
-  //       }
-  //     }
-  //   }
-  // });
-
-  sass: {
-      dist: {
-        files: {
-          'main.css': 'main.scss',
-          'widgets.css': 'widgets.scss'
-        }
-      }
     }
 
-});
-    // sass: {
-    //   dist: {
-    //
-    // //     files:  {'./SASS/style.sass';}{'./build/CSS/sass.css';}
-    //         }
-    //       }
 
+
+  });
 
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-sass');
-  grunt.registerTask("default", ['concat', "watch"]);
-  grunt.registerTask('speak', function() {
-    console.log("im speaking");
-  });
+  grunt.registerTask("default", ['concat', 'sass', "watch"]);
 
 };
