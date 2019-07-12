b44cad2451be2c0d03b641525153a2255193b601
Ray Mercedes <contact@RayMercedes.com>
2019-07-11 21:56:52 -0400

    Tweaked text  and word spacing

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,43 +8,10 @@ module.exports = function (grunt) {
         dest: 'dist/JS/main.js',
       },
       css: {
-        src: ['./library/css/custom_styles.css', './library/css/style.css', './library/css/mobile.css'],
+        src: ['./library/css/main.css', './library/css/style.css', './library/css/mobile.css'],
         dest: 'dist/CSS/Styles.css',
       }
     },
-
-    sass: { // Task
-      dist: { // Target
-        options: { // Target options
-          style: 'expanded'
-        },
-        files: { // Dictionary of files
-          'main.css': 'main.scss' // 'destination': 'source'
-        }
-      }
-    },
-
-    // sass: {
-    //   build: {
-    //     files: [{
-    //           //       '/library/SASS/styles.scss': '/dist/CSS/style.css'
-
-    //       // expand: true,
-    //       // cwd: 'styles',
-    //       // src: ['./library/sass/*.sass'],
-    //       // dest: './dist/CSS/',
-    //       // ext: '.css'
-    //     }]
-    //   }
-    // },
-
-    // sass: {
-    //   dist: {
-    //     files: [{
-    //       '/library/SASS/styles.scss': '/dist/CSS/style.css'
-    //     }]
-    //   }
-    // },
 
 
     watch: {
@@ -64,7 +31,7 @@ module.exports = function (grunt) {
 
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-sass');
-  grunt.registerTask("default", ['concat', 'sass', "watch"]);
+  // grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.registerTask("default", ['concat', "watch"]);
 
 };
