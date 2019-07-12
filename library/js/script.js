a473e79db66833705a2d20b9db1ad4e49cee746a
ray <coolrey3@gmail.com>
2019-07-12 14:17:23 -0400

    script changes

--- library/js/script.js
+++ library/js/script.js
@@ -1,4 +1,4 @@
-<script>
+
         function FormValidation() {
 //First Name Validation 
     var fn=document.getElementById('FirstName').value;
@@ -17,11 +17,73 @@
         document.getElementById('FirstName').style.borderColor = "green";
     }
     if(fn.length <=2){
-        alert('Your Nname is too short');
+        alert('Your Sname is too short');
         document.getElementById('FirstName').style.borderColor = "red";
         return false;
     }else{
         document.getElementById('FirstName').style.borderColor = "green";
     }
+
+
+    //Last Name Validation 
+    var fn=document.getElementById('LastName').value;
+    if(fn == ""){
+        alert('Please Enter Last Name');
+        document.getElementById('LastName').style.borderColor = "red";
+        return false;
+    }else{
+        document.getElementById('LastName').style.borderColor = "green";
+    }
+    if (/^[0-9]+$/.test(document.getElementById("FirstName").value)) {
+        alert("First Name Contains Numbers!");
+        document.getElementById('LastName').style.borderColor = "red";
+        return false;
+    }else{
+        document.getElementById('LastName').style.borderColor = "green";
+    }
+    if(fn.length <=2){
+        alert('Your Nname is too short');
+        document.getElementById('LastName').style.borderColor = "red";
+        return false;
+    }else{
+        document.getElementById('LastName').style.borderColor = "green";
+    }
+
+    //Email Validation 
+    var fn=document.getElementById('Email').value;
+    if(fn == ""){
+        alert('Please Enter Last Name');
+        document.getElementById('Email').style.borderColor = "red";
+        return false;
+    }else{
+        document.getElementById('Email').style.borderColor = "green";
+    }
+    if (/^[0-9]+$/.test(document.getElementById("Email").value)) {
+        alert("First Name Contains Numbers!");
+        document.getElementById('Email').style.borderColor = "red";
+        return false;
+    }else{
+        document.getElementById('Email').style.borderColor = "green";
+    }
+    if(fn.length <=2){
+        alert('Your Nname is too short');
+        document.getElementById('Email').style.borderColor = "red";
+        return false;
+    }else{
+        document.getElementById('Email').style.borderColor = "green";
+    }
+
+
 }
-    </script>
+
+// $(document).ready(function(){
+//     var $form = $('form');
+//     $form.submit(function(){
+//        $.post($(this).attr('action'), $(this).serialize(), function(response){
+//              // do something here on success
+//        },'json');
+//        return false;
+//     });
+//  });
+
+    // </script>
