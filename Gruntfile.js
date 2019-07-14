02aab4a945f8657ec67c18a1fdb889ecded7859f
Ray Mercedes <contact@RayMercedes.com>
2019-07-14 00:58:00 -0400

    minified code

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,3 +1,5 @@
+const mozjpeg = require('imagemin-mozjpeg');
+
 module.exports = function (grunt) {
 
   // Project configuration.
@@ -31,18 +33,60 @@ module.exports = function (grunt) {
     uglify: {
       my_target: {
         files: {
-          'dist/JS/output.min.js': ['./library/js/script.js']
+          'dist/JS/main.min.js': ['./library/js/script.js']
         }
       }
+    },
+
+    imagemin: {
+      static: {
+          options: {
+              optimizationLevel: 3,
+              svgoPlugins: [{removeViewBox: false}],
+              use: [mozjpeg()] // Example plugin usage
+          },
+          files: {
+              'dist/images/cropped_phone1_minified.png': 'library/img/cropped_phone1.png',
+              'dist/images/DT-cropped_min.png': 'library/img/DT-cropped.png',
+              // 'dist/img.gif': 'library/img/img.gif'
+          }
+      },
+
+
+      dynamic: {
+          files: [{
+              expand: true,
+              cwd: '/library/img/',
+              src: ['**/*.{png,jpg,gif}'],
+              dest: 'dist/images/'
+          }]
+      }
+  },
+
+  cssmin: {
+    target: {
+      files: [{
+        expand: true,
+        cwd: 'dist/CSS',
+        src: ['*.css', '!*.min.css'],
+        dest: 'dist/CSS',
+        ext: '.min.css'
+      }]
     }
+  }
   
 
 
   });
 
+  // Load Tasks
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.registerTask("default", ['concat', 'uglify' 'watch']);
+  grunt.loadNpmTasks('grunt-contrib-imagemin');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+
+// Register Default
+  grunt.registerTask("default", ['concat', 'uglify','imagemin', 'watch']);
 
 };
