1f50385a5468fd9b3313ea66803501616c335977
Ray Mercedes <contact@RayMercedes.com>
2019-07-14 03:39:23 -0400

    changed scipts

---
+++ library/js/scrap.js
@@ -0,0 +1,24 @@
+
+
+$(document).ready(function () {
+    $('#MainForm').validate({
+        rules: {
+            email: {
+                required: true,
+                email: true
+            },
+        },
+
+        highlight: function (element, errorClass, validClass) {
+            $(element).nextAll('.form-control-feedback').show().removeClass('glyphicon-ok').addClass('glyphicon-remove');
+            $(element).addClass(errorClass).removeClass(validClass);
+            $(element).closest('.form-group').removeClass('has-success').addClass('has-error');
+        },
+        success: function (element) {
+            $(element).nextAll('.form-control-feedback').show().removeClass('glyphicon-remove').addClass('glyphicon-ok');
+            element.closest('.form-group').removeClass('has-error').addClass('has-success');
+            $(element).remove();
+        }
+    });
+});
+
