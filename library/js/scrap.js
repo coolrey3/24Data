eba50468857e9875caf9b378b8992d64bcc0a445
Ray Mercedes <contact@RayMercedes.com>
2019-07-14 17:45:56 -0400

    updates

--- library/js/scrap.js
+++ library/js/scrap.js
@@ -1,12 +1,59 @@
 
 
-$(document).ready(function () {
+
+document.getElementById('HamBurger').onclick = function(){
+    if(document.getElementById('NavSkill').style.display == "none" ) {
+
+        document.getElementById('NavSkill').style.display = "block";
+    } else {
+        document.getElementById('NavSkill').style.display = "none"
+    };
+
+};
+
+
+$(document).ready(function() {
+
+
+    $.validator.addMethod('length' , function (value,element) {
+        return value.length <= 20;
+    }, 'Your name must be less than 20 characters')
+
     $('#MainForm').validate({
         rules: {
             email: {
                 required: true,
                 email: true
             },
+
+            firstname: {
+                required: true,
+                firstname: true,
+                length: true
+            },
+
+            lastname: {
+                required: true,
+                lastname: true,
+                length: true
+            },
+
+            zip: {
+                required: true,
+                zip: true
+            },
+
+            phone: {
+                required: true,
+                phone: true
+            },
+        },
+
+        messages: {
+            email: {
+                required: 'Please enter an email address',
+                email: 'Please enter a <em>valid</em> email address.'
+            }
         },
 
         highlight: function (element, errorClass, validClass) {
