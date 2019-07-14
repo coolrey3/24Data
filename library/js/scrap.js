d44919b32b8eb26d015dd95fb6f0e358e5939466
Ray Mercedes <contact@RayMercedes.com>
2019-07-14 19:42:30 -0400

    validation testing

--- library/js/scrap.js
+++ library/js/scrap.js
@@ -1,8 +1,5 @@
-
-
-
-document.getElementById('HamBurger').onclick = function(){
-    if(document.getElementById('NavSkill').style.display == "none" ) {
+document.getElementById('HamBurger').onclick = function () {
+    if (document.getElementById('NavSkill').style.display == "none") {
 
         document.getElementById('NavSkill').style.display = "block";
     } else {
@@ -11,61 +8,88 @@ document.getElementById('HamBurger').onclick = function(){
 
 };
 
+$.validator.addMethod("length", function (value, element) {
+    return value.length <= 20;
+}, 'Your name must be less than 20 characters')
 
-$(document).ready(function() {
+
+$(document).ready(function () {
+
+    $('#MainForm').validate(
+
+        {
+            // rules: {
+            //     email: {
+            //         required: true,
+            //         email: true
+            //     },
+
+            //     firstname: {
+            //         required: true,
+            //         firstname: true,
+            //         length: true
+            //     },
+
+            //     lastname: {
+            //         required: true,
+            //         lastname: true,
+            //         length: true
+            //     },
+
+            //     zip: {
+            //         required: true,
+            //         zip: true
+            //     },
+
+            //     phone: {
+            //         required: true,
+            //         phone: true
+            //     },
+
+            //     consentbox: {
+            //         required: true,
+            //         consentbox: true
+            //     }
 
 
-    $.validator.addMethod('length' , function (value,element) {
-        return value.length <= 20;
-    }, 'Your name must be less than 20 characters')
+            // },
 
-    $('#MainForm').validate({
-        rules: {
-            email: {
-                required: true,
-                email: true
+            messages: {
+                email: {
+                    required: 'Please enter an email address',
+                    email: 'Please enter a <em>valid</em> email address.'
+                },
+
+                firstname: {
+                    required: 'Please enter your first name'
+                },
+
+                lastname: {
+                    required: 'Please enter your last name'
+                },
+
+                phone: {
+                    required: 'Please enter your phone'
+                },
+
+                zip: {
+                    required: 'Please enter your zip code'
+                },
+
+                consentbox: {
+                    required: 'Consent required!'
+                }
             },
 
-            firstname: {
-                required: true,
-                firstname: true,
-                length: true
+            highlight: function (element, errorClass, validClass) {
+                $(element).nextAll('.form-control-feedback').show().removeClass('glyphicon-ok').addClass('glyphicon-remove');
+                $(element).addClass(errorClass).removeClass(validClass);
+                $(element).closest('.form-group').removeClass('has-success').addClass('has-error');
             },
-
-            lastname: {
-                required: true,
-                lastname: true,
-                length: true
-            },
-
-            zip: {
-                required: true,
-                zip: true
-            },
-
-            phone: {
-                required: true,
-                phone: true
-            },
-        },
-
-        messages: {
-            email: {
-                required: 'Please enter an email address',
-                email: 'Please enter a <em>valid</em> email address.'
+            success: function (element) {
+                $(element).nextAll('.form-control-feedback').show().removeClass('glyphicon-remove').addClass('glyphicon-ok');
+                element.closest('.form-group').removeClass('has-error').addClass('has-success');
+                $(element).remove();
             }
-        },
-
-        highlight: function (element, errorClass, validClass) {
-            $(element).nextAll('.form-control-feedback').show().removeClass('glyphicon-ok').addClass('glyphicon-remove');
-            $(element).addClass(errorClass).removeClass(validClass);
-            $(element).closest('.form-group').removeClass('has-success').addClass('has-error');
-        },
-        success: function (element) {
-            $(element).nextAll('.form-control-feedback').show().removeClass('glyphicon-remove').addClass('glyphicon-ok');
-            element.closest('.form-group').removeClass('has-error').addClass('has-success');
-            $(element).remove();
-        }
-    });
+        });
 });
-
